2d123dfba89e8fe47888045d10da6c700e0e8bdc
asad-aali <asadaali@stanford.edu>
2025-06-03 17:23:41 -0700

    flake8

--- src/helm/clients/dspy_client.py
+++ src/helm/clients/dspy_client.py
@@ -10,6 +10,7 @@ import os
 lm = None
 agent = None
 
+
 class DSPyClient(Client):
     """
     A HELM client that uses DSPy for inference instead of directly calling the model.
@@ -22,7 +23,7 @@ class DSPyClient(Client):
         cache_config: CacheConfig,
         model_name: str = None,
         api_base: str = None,
-        api_key: str = None
+        api_key: str = None,
     ):
         """
         Initializes the DSPyClient.
@@ -39,16 +40,18 @@ class DSPyClient(Client):
         model_name = os.environ.get("DSPY_MODEL_NAME", None)
         api_base = os.environ.get("DSPY_API_BASE", None)
         api_key = os.environ.get("DSPY_API_KEY", None)
-        
+
         if (model_name == None) or (api_base == None) or (api_key == None):
-            raise Exception("\n\nPlease add the model name, api base, and api key for your DSPy program in your environment variables.\n\nexport DSPY_MODEL_NAME=YOUR_MODEL_NAME\nexport DSPY_API_BASE=YOUR_BASE_URL\nexport DSPY_API_KEY=YOUR_API_KEY\n\n")
-        
+            raise Exception(
+                "\n\nPlease add the model name, api base, and api key for your DSPy program in your environment variables.\n\nexport DSPY_MODEL_NAME=YOUR_MODEL_NAME\nexport DSPY_API_BASE=YOUR_BASE_URL\nexport DSPY_API_KEY=YOUR_API_KEY\n\n"
+            )
+
         global lm, agent
         if lm == None:
             lm = dspy.LM(model=model_name, api_base=api_base, api_key=api_key)
             dspy.configure(lm=lm)
             agent = dspy.Predict("inputs -> answer")
-        
+
         self.cache = Cache(cache_config) if cache_config else None
 
     def make_request(self, request: Request) -> RequestResult:
